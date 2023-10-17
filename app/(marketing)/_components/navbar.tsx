c1c17fa1970385b0afced69c9f196dfcfdcd7731
Kalpesh Khairnar <71657135+khairnarkalpesh@users.noreply.github.com>
2023-10-17 15:33:25 +0530

    authentication added and database setup

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -4,14 +4,50 @@ import { cn } from '@/lib/utils';
 import React from 'react'
 import Logo from './logo';
 import { ModeToggle } from '@/components/ui/mode-toggle';
+import { useConvexAuth } from 'convex/react';
+import { SignInButton, UserButton } from '@clerk/clerk-react';
+import { Button } from '@/components/ui/button';
+import { Spinner } from '@/components/ui/spinner';
+import Link from 'next/link';
 
 const Navbar = () => {
     const scrolled = useScrollTop();
+    const {isAuthenticated, isLoading} = useConvexAuth();
+
   return (
     <div className={cn("z-50 bg-background fixed top-0 flex items-center w-full p-6 dark:bg-[#1f1f1f]", scrolled && "border-b shadow-sm")}>
         <Logo />
 
         <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+            {isLoading && (
+                <Spinner />
+            )}
+            {!isAuthenticated && !isLoading && (
+                <>
+                <SignInButton mode='modal'>
+                    <Button variant="ghost" size="sm">
+                        Log In
+                    </Button>
+                </SignInButton>
+                <SignInButton mode='modal'>
+                    <Button size="sm">
+                        Get Notion free
+                    </Button>
+                </SignInButton>
+                </>
+            )}
+            {isAuthenticated && !isLoading && (
+                <>
+                <Button variant="ghost" size="sm" asChild>
+                    <Link href="/documents">
+                        Enter Notion
+                    </Link>
+                </Button>
+                <UserButton
+                afterSignOutUrl='/' 
+                />
+                </>
+            )}
             <ModeToggle />
         </div>
     </div>
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain:"https://up-walleye-94.clerk.accounts.dev",
+            applicationID: "convex"
+        }
+    ]
+}
